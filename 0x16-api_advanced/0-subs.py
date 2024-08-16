08f61140e449c95857d289f04ba0df961746e5e7
Junioromondi <omondi.junior@yahoo.com>
2024-08-16 14:22:49 +0300

    code correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,8 +9,14 @@ def number_of_subscribers(subreddit):
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data["data"]["subscribers"]
+        else:
+            return 0
+    except Exception:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
